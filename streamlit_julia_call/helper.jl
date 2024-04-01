e4bed05613ffc3a821586493dab89e9a917483de
Kenta Murata <mrkn@mrkn.jp>
2024-04-01 15:13:51 +0900

    WIP

--- streamlit_julia_call/helper.jl
+++ streamlit_julia_call/helper.jl
@@ -3,15 +3,17 @@ module StreamlitHelper
 import PyCall
 import PyCall: PyPtr, PyNULL, PyMethodDef, METH_O, @pysym, @pycheckn, pyincref_, pynothing, ispynull
 
-# The mapping from a weakref of Python module to the
-# corresponding Julia module
-const script_module_cache = Dict{PyPtr,Module}()
+# The mapping from a sessionid to the corresponding weakref of Python module
+const session_weakmodule_cache = Dict{String,PyPtr}()
+
+# The mapping from a weakref of Python module to the corresponding Julia module
+const julia_script_module_cache = Dict{PyPtr,Module}()
 
 # weakref_callback Python method
 const script_module_weakref_callback_obj = PyNULL()
 
-# Python expects the PyMethodDef structure to be a constant, so
-# we put it in a global to prevent GC.
+# Python expects the PyMethodDef structure to be a constant, so we put it in a
+# global to prevent GC.
 const script_module_weakref_callback_method = Ref{PyMethodDef}()
 
 function script_module_removal_callback(callback::PyPtr, weakref::PyPtr)
@@ -24,7 +26,7 @@ function ispythonmodule(pyobj::PyPtr)
     true  # FIXME
 end
 
-function get_script_module(py_script_module::PyPtr)
+function get_script_module(sessionid::String, py_script_module::PyPtr)
     if !ispythonmodule(py_script_module)
         throw(ArgumentError("get_script_module: a Python module is required"))
     end
@@ -38,15 +40,34 @@ function get_script_module(py_script_module::PyPtr)
                                        script_module_weakref_callback_method, C_NULL, C_NULL)))
     end
 
-    wo = @pycheckn ccall((@pysym :PyWeakRef_New), PyPtr, (PyPtr, PyPtr),
-                         py_script_module, script_module_weakref_callback_obj)
-    mod = Module()
-    script_module_cache[wo] = mod
-    return mod
+    if haskey(session_weakmodule_cache, sessionid)
+        weakmod = session_weakmodule_cache[sessionid]
+        pymod = @pycheckn ccal((@pysym :PyWeakRef_GetObject), PyPtr, (PyPtr,), weakmod)
+
+        if py_script_module !== pymod
+            # On the case that the new python module is attached to the given session,
+            # we need to replace the script running module in Julia side
+            delete!(script_module_cache, weakmod)
+            delete!(session_weakmodule_cache, sessionid)
+            return get_script_module(sessionid, py_script_module)
+        end
+    end
+
+    if !haskey(session_weakmodule_cache, sessionid)
+        weakmod = @pycheckn ccall((@pysym :PyWeakRef_New), PyPtr, (PyPtr, PyPtr),
+                                  py_script_module, script_module_weakref_callback_obj)
+        session_weakmodule_cache[sessionid] = weakmod
+    end
+
+    if haskey(script_module_cache, weakmod)
+        script_module_cache[weakmod]
+    else
+        script_module_cache[weakmod] = Module()
+    end
 end
 
-function eval_for_session(py_script_module::PyPtr, str::String)
-    mod = get_script_module(py_script_module)
+function eval_for_session(sessionid::String, py_script_module::PyPtr, str::String)
+    mod = get_script_module(sessionid, py_script_module)
     ast = Meta.parse("begin\n$(str)\nend")
     mod.eval(ast)
 end
